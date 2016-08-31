56c59f9441e53281f897acc4aaf084351fe862a3
Frédéric Guérin <frederickjeanguerin@hotmail.com>
2016-08-31 11:20:15 -0400

    REFACTOR lexer

--- lexing/lexer.py
+++ lexing/lexer.py
@@ -17,48 +17,58 @@ def tokens_from(source):
     f = CharFeeder(source)      
     while not f.is_empty():
         # Skip whitespace
-        while f.current.isspace():
-            f.next()
-        if f.is_empty() :
-            break;    
-        # Else a new token starts here    
-        f.start_token()
-        # LLVM Identifier 
-        if f.current == "%":
-            iden_type = LlvmIdentifier
-            f.next()
-        else:
-            iden_type = Identifier    
-        # Identifier 
-        if f.current.isalpha():
-            while f.current.isalnum() or f.current == '_':
+        if f.current.isspace():
+            while f.current.isspace():
                 f.next()
-            yield f.new_token(iden_type)
-            continue;
-        else:
-            f.rebase()    
-        # Number
-        if f.current.isdigit() or f.current == '.':
-            while f.current.isdigit() or f.current == '.':
-                f.next()
-            yield f.new_token(Number)
-        # Line Comment --> skip
-        elif f.current == '#':
+            continue    
+
+        # Skip line comments
+        if f.current == '#':
             while f.current and f.current not in '\r\n':
                 f.next()
-        # Operator 
-        elif is_operator_char(f.current):
-            while is_operator_char(f.current):
-                f.next()
-            yield f.new_token(Operator)
-        # Punctuator
-        else:    
-            f.next()
-            yield f.new_token(Punctuator)
+            continue    
+
+        # Else a new token starts here    
+        f.start_token()
+        yield find_token(f)
 
     # Yield a terminal EOF marker        
     f.start_token()
     yield f.new_token(EOF)
+
+
+def find_token(f):
+
+    # LLVM Identifier 
+    if f.current == "%":
+        iden_type = LlvmIdentifier
+        f.next()
+    else:
+        iden_type = Identifier    
+
+    # Identifier 
+    if f.current.isalpha():
+        while f.current.isalnum() or f.current == '_':
+            f.next()
+        return f.new_token(iden_type)
+    else:
+        f.rebase()    
+
+    # Number
+    if f.current.isdigit() or f.current == '.':
+        while f.current.isdigit() or f.current == '.':
+            f.next()
+        return f.new_token(Number)
+
+    # Operator 
+    if is_operator_char(f.current):
+        while is_operator_char(f.current):
+            f.next()
+        return f.new_token(Operator)
+
+    # Punctuator
+    f.next()
+    return f.new_token(Punctuator)
 
 
 
